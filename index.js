cec473269d8ac18073e57301f8e61cd00142bba1
Tilak-Reddy <163108070+Tilak-Reddy@users.noreply.github.com>
2024-12-13 07:19:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const newsdetails = document.getElementById("newsdetails");
 var newsDataArr = [];
 
 // apis 
-const API_KEY = "YOUR_API_KEY";
+const API_KEY = "bb8ca625cbb9472f92ffc37368189259";
 const HEADLINES_NEWS = "https://newsapi.org/v2/top-headlines?country=in&apiKey=";
 const GENERAL_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=general&apiKey=";
 const BUSINESS_NEWS = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=";
